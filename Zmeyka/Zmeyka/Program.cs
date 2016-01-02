ee69d9399fcb086e9bbaccf803ef95c541015b9a
AZebra <fimezebra@gmail.com>
2016-01-02 19:25:34 +0200

    Created Class Point.

--- Zmeyka/Zmeyka/Program.cs
+++ Zmeyka/Zmeyka/Program.cs
@@ -9,17 +9,19 @@ namespace Zmeyka
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 28;
+            p1.y = 4;
+            p1.sym = '#';
+            p1.Draw();
 
-            Draw(2, 6, '#');
-            
+            Point p2 = new Point();
+            p2.x = 15;
+            p2.y = 14;
+            p2.sym = '*';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
         }
     }
 }
