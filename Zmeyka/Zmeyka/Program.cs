a89b50a86acf21623a3f6135087192c4d7a15ed5
AZebra <fimezebra@gmail.com>
2016-01-02 21:59:39 +0200

    Created lists

--- Zmeyka/Zmeyka/Program.cs
+++ Zmeyka/Zmeyka/Program.cs
@@ -15,6 +15,21 @@ namespace Zmeyka
             Point p2 = new Point(15, 14, '*');
             p2.Draw();
 
+            Point p3 = new Point(8, 8, '^');
+            p3.Draw();
+
+            Point p4 = new Point(30, 1, '&');
+            p4.Draw();
+            
+            List<Point> Plist = new List<Point>();
+            Plist.Add( p1 );
+            Plist.Add( p2 );
+            Plist.Add( p3 );
+            Plist.Add( p4 );
+
+           
+            
+
             Console.ReadLine();
         }
     }
